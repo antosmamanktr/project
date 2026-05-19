ef6d2e2026337fafd3eda4cbc5873108087c1da1
Antos Maman <antosmamanktr@gmail.com>
2026-05-19 22:39:29 +0530

    Rebase commit 2

--- plp.js
+++ plp.js
@@ -1,8 +1,10 @@
 // plp.js
 console.log("PLP Screen");
 console.log("Product Screen added");
-var ab=123;
 var a=1;
+var b =2;
+var vb='visuals';
+var ab=123;
 var Str="I am a JOAT"
 var c=213;
-var b =2;
+var d=12345
